2c47c0466eb215ccdc15b2b1490a4f52a39f615f
Kunc <kurookun@gmail.com>
2020-12-23 07:27:26 +0000

    first commit by kunc

--- server.js
+++ server.js
@@ -30,3 +30,6 @@ app.get("/api/hello", function (req, res) {
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+
